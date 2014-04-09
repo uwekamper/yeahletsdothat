a99541e94c005dd86c95ff5ea53b4598668234fa
Uwe Kamper <me@uwekamper.de>
2014-04-09 21:04:22 +0200

    remove some unused code

--- src/yldt_braintree/urls.py
+++ src/yldt_braintree/urls.py
@@ -7,6 +7,4 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^(?P<transaction_pk>\d+)/$', 'yldt_braintree.views.payment_form', name='yldt_braintree_payment_form'),
     url(r'^success/$', 'yldt_braintree.views.payment_succes', name='yldt_braintree_payment_success'),
-    # url(r'^(?P<key>\w+)/$', 'campaigns.views.campaign_details', name='campaign_details'),
-    # url(r'^(?P<key>\w+)/pay/$', 'campaigns.views.select_payment', name='select_payment'),
 )

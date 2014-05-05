4399920e8d5bdbd332d3de6b634b4d869b3d4a98
Uwe Kamper <me@uwekamper.de>
2014-05-05 18:48:58 +0200

    reorder some of the configuration files

--- src/yldt_bitcoin/bitcoin.py
+++ src/yldt_bitcoin/bitcoin.py
@@ -5,5 +5,13 @@ from campaigns.payment_method import BasePaymentMethod
 
 
 class BitCoin(BasePaymentMethod):
-    name = 'bitcoin'
-    display_name = 'Bitcoin'
+
+    def __init__(self, options):
+        super(BitCoin, self).__init__(options)
+
+    def pay(self, campaign, transaction):
+        # TODO implement this
+        pass
+
+    def refund(self, campaign, transaction):
+        pass
--- src/yldt_braintree/__init__.py
+++ src/yldt_braintree/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from method import BrainTree as PaymentMethod
+from braintree import BrainTree as PaymentMethod
